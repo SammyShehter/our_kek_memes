444545cf68219ba9c10c953918cf2d03c1e2259f
sammyshehter <samir_shehter@yahoo.com>
2023-08-04 15:28:48 +0300

    v1 done. no edits

--- scrap.py
+++ scrap.py
@@ -1,63 +1,80 @@
+import os
+import json
+import time
 import cssutils
+from datetime import datetime
 from bs4 import BeautifulSoup
 import requests
-import time
-import json
-from datetime import datetime
-import os
 
 
-def write_to_json(filepath, jsonData):
+def write_to_json(filepath, json_data):
     with open(filepath, "w") as json_file:
-        json.dump(jsonData, json_file)
+        json.dump(json_data, json_file)
 
 
 def read_json_file(file_path):
     with open(file_path, 'r') as json_file:
-        dump = json.load(json_file)
-        return dump
+        return json.load(json_file)
 
 
-raw_list = read_json_file('./list.json')
-parsed_keys = raw_list.keys()
-channels = list(parsed_keys)
+raw_data = read_json_file('./list.json')
+channels = list(raw_data.keys())
 base_url = "https://t.me/"
 memes_dictionary = {}
 current_datetime = datetime.now().strftime('%d.%m.%Y_%H:%M')
 
 for channel in channels:
     working = True
-    lastPost = raw_list[channel]
+    last_post = raw_data[channel]
+    times_moved_to_next_post = 6
 
     while working:
         time.sleep(10)
-        html_text = requests.get(base_url + channel + '/' +
-                                 str(lastPost)+'?embed=1&mode=tme').text
-
-        soup = BeautifulSoup(html_text, 'lxml')
         try:
-            post = soup.find('a', class_="tgme_widget_message_photo_wrap")
+            html_text = requests.get(
+                f"{base_url}{channel}/{last_post}?embed=1&mode=tme").text
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            break
+
+        last_post += 1
+        soup = BeautifulSoup(html_text, 'lxml')
+        post = soup.find('a', class_="tgme_widget_message_photo_wrap")
+
+        try:
             if not post:
+                post_is_video = soup.find(
+                    'video', class_="tgme_widget_message_video")
+                if post_is_video:
+                    times_moved_to_next_post = 6
+                    video_src = post_is_video.get('src')
+                    exit_status = os.system(
+                        f"grep -r -l '{video_src}' ./archive/*.json")
+                    if exit_status == 0:
+                        continue
+                    memes_dictionary[f'{channel}_{last_post}'] = video_src + '\n'
+                    continue
                 post_not_found = soup.find(
                     'div', class_="tgme_widget_message_error")
                 if post_not_found and post_not_found.text == "Post not found":
-                    raw_list[channel] = lastPost
-                    working = 0
-                lastPost += 1
-                continue
-            lastPost += 1
-            style = cssutils.parseStyle(post['style'])
-            url = style['background-image'].replace(
-                'url(', '').replace(')', '')
-            exit_status = os.system(f"grep -r -l '{url}' ./archive/*.json")
-            if exit_status == 0:
-                continue
-            memes_dictionary[f'{channel}_{lastPost}'] = url+'\n'
+                    if times_moved_to_next_post == 0:
+                        last_post -= 6
+                        raw_data[channel] = last_post
+                        working = False
+                    times_moved_to_next_post -= 1
+                last_post += 1
+            else:
+                times_moved_to_next_post = 6
+                style = cssutils.parseStyle(post['style'])
+                url = style['background-image'].strip('url()')
+                exit_status = os.system(f"grep -r -l '{url}' ./archive/*.json")
+                if exit_status == 0:
+                    continue
+                memes_dictionary[f'{channel}_{last_post}'] = url + '\n'
         except Exception as e:
-            raw_list[channel] = lastPost
-            working = 0
-
-write_to_json('./list.json', raw_list)
-
+            print(f"An error occurred: {e}")
+            raw_data[channel] = last_post
+            working = False
+write_to_json('./list.json', raw_data)
 if memes_dictionary:
     write_to_json('./archive/' + current_datetime + '.json', memes_dictionary)
