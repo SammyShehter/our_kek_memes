0ef4203a357bfed7b6acdc86bc73a35887f393b9
sammy <sammy@payplus.co.il>
2023-10-18 11:49:39 +0300

    saving videos

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import requests
 import hashlib
 
+
 def save_content_to_file(folder, filename, content):
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -9,24 +10,33 @@ def save_content_to_file(folder, filename, content):
     with open(os.path.join(folder, filename), 'wb') as f:
         f.write(content)
 
-def get_video_hash(url):
+
+def get_video_hash(url, save_folder=None):
     response = requests.get(url, stream=True)
-    
+
     hash_object = hashlib.sha256()
-    
+
+    video_chunks = []
+
     for chunk in response.iter_content(chunk_size=4096):
+        video_chunks.append(chunk)
         hash_object.update(chunk)
 
     hex_dig = hash_object.hexdigest()
+
+    video_content = b"".join(video_chunks)
+    save_content_to_file("videos", hex_dig, video_content)
+
     return hex_dig
+
 
 def get_image_hash(url):
     response = requests.get(url)
     image_content = response.content
-    
+
     hash_object = hashlib.sha256(image_content)
     hex_dig = hash_object.hexdigest()
 
     save_content_to_file("images", hex_dig, image_content)
-    
+
     return hex_dig
